bde9f3057ecfd3e24ce29b6920e05694f394d5da
adyakbar <muh.adyakbar@gmail.com>
2016-09-23 14:10:57 +0700

    menganti welcome jadi adi ganteng

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sajaa Mantaaapp NiHHH!!!!
+                    Adi Ganteng
                 </div>
 
                 <div class="links">
