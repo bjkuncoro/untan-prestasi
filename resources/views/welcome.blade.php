c2547bac13cac351be9d092b695a2dc95626c2dc
adyakbar <muh.adyakbar@gmail.com>
2016-09-23 14:14:13 +0700

    mencoba biar bentrok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Adi Ganteng
+                    Ini Akan Bentrok
                 </div>
 
                 <div class="links">
